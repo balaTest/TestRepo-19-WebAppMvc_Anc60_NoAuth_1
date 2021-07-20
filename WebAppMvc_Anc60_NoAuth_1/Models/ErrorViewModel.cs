25be6dbc1188b313561ad98ea5bac021a112a831
balach <balach@19vcazhH1003>
2021-07-20 06:58:44 -0700

    Add project files.

---
+++ WebAppMvc_Anc60_NoAuth_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+
+namespace WebAppMvc_Anc60_NoAuth_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
